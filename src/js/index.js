a168d2ee8eb3c3bead6969786266258121f3b791
davistheweb <smartkelvin775@gmail.com>
2024-03-25 15:40:24 +0100

    new changes

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+// DARK THEME
+
+
+darkTheme = () => {
+    let theme = document.getElementById('theme'),
+        themeIcon = document.querySelector('.theme-icons')
+
+    // Function to set the theme
+    const setTheme = (isDarkTheme) => {
+        if (isDarkTheme) {
+            document.body.classList.add('dark-theme');
+        } else {
+            document.body.classList.remove('dark-theme');
+        }
+    }
+
+    // Check if there's a theme preference in localStorage
+    const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+    setTheme(isDarkTheme);
+
+    // Event listener for toggling dark theme
+    theme.addEventListener('click', () => {
+        const isDark = document.body.classList.toggle('dark-theme');
+        localStorage.setItem('darkTheme', isDark);
+    });
+
+    // Event listener for toggling theme icon
+    themeIcon.addEventListener('click', () => {
+        document.body.classList.toggle('change-theme-icon');
+    });
+}
+
+darkTheme();
