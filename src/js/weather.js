e048024d2b4b53192906f629e2d24f3f602a7340
davistheweb <smartkelvin775@gmail.com>
2024-03-27 15:16:05 +0100

    added api url and token

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,5 +1,5 @@
 const Weatherapi_key = "6b7f9e185eb663f62a87135fb27ea932"
-const weatherApi_url = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=nigeria"
+const weatherApi_url = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q="
 const search_input = document.querySelector('.weather-input')
 const search_btn = document.querySelector('.search-button')
     
@@ -8,9 +8,9 @@ async function loadWeather(City) {
     var Weatherdata = await rspd.json();
 
     document.querySelector('.weather-country').textContent = Weatherdata.name;
-    document.querySelector(".temp").textContent = Math.round (Weatherdata.main.temp) + "°C"
+    document.querySelector(".temp").textContent = Math.round(Weatherdata.main.temp) + "°C"
 
-    console.log(Weatherdata);
+    
 }
 
 search_btn.addEventListener('click', () => {
